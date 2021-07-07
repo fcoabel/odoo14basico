4fddd90abcd81980475e8813d69ca4cbb072d27c
luissancar <luissancar@yahoo.es>
2021-07-07 19:19:02 +0200

    inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+Ejemplo básico odoo 14
---
+++ models/models.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class ejemplo(models.Model):
+#     _name = 'ejemplo.ejemplo'
+#     _description = 'ejemplo.ejemplo'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
+
+
+from odoo import models, fields, api
+
+class persona(models.Model):
+	_name = 'ejemplo.persona'
+	_description = 'model persona'
+
+	name = fields.Char('DNI',required=True)
+	nombre = fields.Char(string='Nombre',required=True)
+	telefono = fields.Char(string='Teléfono',required=True)
