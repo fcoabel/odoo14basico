21cd5db4a553b3b43d0c2f05d9181dadf57eae81
luissancar <luissancar@yahoo.es>
2021-07-13 16:54:37 +0200

    icon

--- __manifest__.py
+++ __manifest__.py
@@ -29,6 +29,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'report/ejemplo_persona_report.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
